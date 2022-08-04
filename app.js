20d360db9e2cc35768577812f0f594fd1b5ceb38
ModestFun <wangziyang@uino.com>
2022-08-04 21:18:50 +0800

    feat: 链接转发

--- app.js
+++ app.js
@@ -6,22 +6,25 @@ const express = require('express');
 const app = express();
 
 const options = {
-    key: fs.readFileSync(path.resolve('./cert/modestfun.top.key')),
-    cert: fs.readFileSync(path.resolve('./cert/modestfun.top_bundle.pem'))
+  key: fs.readFileSync(path.resolve('./cert/modestfun.top.key')),
+  cert: fs.readFileSync(path.resolve('./cert/modestfun.top_bundle.pem'))
 }
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(options, app);
 
-httpServer.listen(80, () => {
-    console.log('HTTP running 80');
+httpServer.listen(8080, () => {
+  console.log('HTTP running 80');
 })
 
 httpsServer.listen(443, () => {
-    console.log('HTTPS running 443')
+  console.log('HTTPS running 443')
 })
 
 app.get('/', (req, res) => {
+  res.send('NB');
+})
 
-    res.send('NB');
+app.get('/muyi-node', (req, res) => {
+  res.redirect('https://service-hwdubjb5-1257118150.sh.apigw.tencentcs.com/link/n3HXan7nq0ZcWh3U?');
 })
